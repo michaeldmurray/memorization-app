a4232c210ceff08db6d1ad711006a8f5f1cc5a8c
michaeldmurray <michaeldavidmurray@gmail.com>
2018-11-12 20:45:36 -0600

    add Question Activity skeleton

--- Micvalmoy/app/src/main/java/csc472/depaul/edu/micvalmoy/activities/QuestionActivity.java
+++ Micvalmoy/app/src/main/java/csc472/depaul/edu/micvalmoy/activities/QuestionActivity.java
@@ -2,6 +2,11 @@ package csc472.depaul.edu.micvalmoy.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.text.Format;
+
+import csc472.depaul.edu.micvalmoy.R;
 
 public class QuestionActivity extends DescendantActivity {
 
@@ -10,14 +15,48 @@ public class QuestionActivity extends DescendantActivity {
     public static final String MODE = "question mode";
     public static final String ID = "question id";
 
+    private Format format;
+
     public enum mode {
         QUIZ,
         PRACTICE,
         RESULT
     }
 
+    //TODO replace this with question format from Question model
+    private enum Format {
+        TRUE_FALSE,
+        MULTI_CHOICE,
+        MULTI_ANSWER,
+        FILL_IN_THE_BLANK
+    }
+
+    //TODO replace this with actual analysis of Question instance
+    private Format getQuestionFormat(int question) {
+        switch (question) {
+            case 1:
+                return Format.TRUE_FALSE;
+            case 2:
+                return Format.MULTI_CHOICE;
+            case 3:
+                return Format.MULTI_ANSWER;
+            case 4:
+                return Format.FILL_IN_THE_BLANK;
+            default:
+                Log.e("QuestionFormat","This should not be reached. returning T/F");
+                return Format.TRUE_FALSE;
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+        //need to pull in xml skeleton when built out.
+        //insert question text.
+        //obtain questionFormat
+        //insert question type text.
+        //delegate to specific onCreates based on FORMAT
+        //delegate to specific onCreates based on MODE
     }
 }
--- Micvalmoy/app/src/main/java/csc472/depaul/edu/micvalmoy/activities/QuizResultActivity.java
+++ Micvalmoy/app/src/main/java/csc472/depaul/edu/micvalmoy/activities/QuizResultActivity.java
@@ -16,26 +16,29 @@ public class QuizResultActivity extends DescendantActivity {
 
         // TODO replace these with programmatic rows
         final TableRow questionRow1 = findViewById(R.id.row_quiz_result);
-        if (questionRow1 != null) { questionRow1.setOnClickListener(onClickQuestionRow);}
+        if (questionRow1 != null) { questionRow1.setOnClickListener(buildOnClickQuestionRow(1));}
 
         final TableRow questionRow2 = findViewById(R.id.row_quiz_result2);
-        if (questionRow2 != null) { questionRow2.setOnClickListener(onClickQuestionRow);}
+        if (questionRow2 != null) { questionRow2.setOnClickListener(buildOnClickQuestionRow(2));}
 
         final TableRow questionRow3 = findViewById(R.id.row_quiz_result3);
-        if (questionRow3 != null) { questionRow3.setOnClickListener(onClickQuestionRow);}
+        if (questionRow3 != null) { questionRow3.setOnClickListener(buildOnClickQuestionRow(3));}
 
         final TableRow questionRow4 = findViewById(R.id.row_quiz_result4);
-        if (questionRow4 != null) { questionRow4.setOnClickListener(onClickQuestionRow);}
+        if (questionRow4 != null) { questionRow4.setOnClickListener(buildOnClickQuestionRow(4));}
     }
 
-    private View.OnClickListener onClickQuestionRow = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(view.getContext(), QuestionActivity.class);
-            Bundle questionBundle = new Bundle();
-            questionBundle.putString(QuestionActivity.MODE,QuestionActivity.mode.RESULT.name());
-            questionBundle.putInt(QuestionActivity.ID,1);
-            startActivity(intent);
-        }
-    };
+    private View.OnClickListener buildOnClickQuestionRow(final int questionID) {
+        View.OnClickListener onClickQuestionRow = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), QuestionActivity.class);
+                Bundle questionBundle = new Bundle();
+                questionBundle.putString(QuestionActivity.MODE, QuestionActivity.mode.RESULT.name());
+                questionBundle.putInt(QuestionActivity.ID, questionID);
+                startActivity(intent);
+            }
+        };
+        return onClickQuestionRow;
+    }
 }
